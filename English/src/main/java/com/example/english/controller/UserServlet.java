6689686b47010f3a1843112ea9f3dc721e0babe2
duydd-c0623g1 <daoducduy.work@gmail.com>
2023-10-02 16:03:57 +0700

    Update

--- English/src/main/java/com/example/english/controller/UserServlet.java
+++ English/src/main/java/com/example/english/controller/UserServlet.java
@@ -34,7 +34,7 @@ public class UserServlet extends HttpServlet {
     }
 
     private void homePage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/home.jsp");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/homePage.jsp");
         requestDispatcher.forward(request, response);
     }
 }
